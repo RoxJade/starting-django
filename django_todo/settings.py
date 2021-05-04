d4009424abf32513aae035b946f8d30f35e8bdfb
RoxJade <roxannejade88@icloud.com>
2021-05-04 12:00:56 +0000

    Setup environment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,23 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
+import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j3pwhjo^wnj49xc_10-j(8srcznyjopg&6q8rbi)=_wl&y$&sb'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-j3pwhjo^wnj49xc_10-j(8srcznyjopg&6q8rbi)=_wl&y$&sb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rox-django-todo-app.herokuapp.com']
+# ALLOWED_HOSTS = ['rox-django-todo-app.herokuapp.com']
+
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -83,7 +89,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://rxdxuditgxauyz:47dcac0f082a91e6ae1fdbc9bf62b3e6db36bfa9636e9a0e580e7cdd5d7b3aff@ec2-54-224-194-214.compute-1.amazonaws.com:5432/d2stli6717a9u6')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
